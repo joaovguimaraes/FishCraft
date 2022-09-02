a7d3a8829bb67d07fd6ba9c47141112908ecc0fa
Joao Victor Guimaraes <57049880+joaovguimaraes@users.noreply.github.com>
2022-09-02 17:35:41 -0300

    Added worm item
    added worm item

---
+++ src/main/java/com/jailmonkeys/fishcraft/item/ModItems.java
@@ -0,0 +1,22 @@
+package com.jailmonkeys.fishcraft.item;
+
+import com.jailmonkeys.fishcraft.FishCraft;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems {
+
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, FishCraft.MOD_ID);
+
+
+    public static final RegistryObject<Item> WORM = ITEMS.register("worm",
+            () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MISC)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
